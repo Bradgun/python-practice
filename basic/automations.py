8462776db1655b776e22d520fa9459ec10da6ccc
Bradgun <72219553+Bradgun@users.noreply.github.com>
2025-06-09 11:37:53 -0700

    day 1

---
+++ basic/automations.py
@@ -0,0 +1,8 @@
+# 5.28.2025
+# This file will show examples of conditional statements
+
+# Create a conditional
+operating_system = "OS 2"
+
+if operating_system == "OS 2":
+    print("Updates needed")
---
+++ basic/coreComponents.py
@@ -0,0 +1,49 @@
+# 5.28.2025
+# Print Hello World!
+# String
+print("Hello World!\n")
+
+# Calculate numbers with print
+# Integer
+print("Value of 1+1:")
+print(1+1)
+print()
+
+# determine boolean values
+# Boolean
+print("True or False")
+print(10<5)
+print(9<12)
+print()
+
+# Print a list
+# List
+print("Fruit List")
+print(["apple", "banana", "carrot"])
+print()
+
+# Use a variable to store a device ID
+# Variable
+device_id = "h43b2"
+print("Device ID:")
+# Difference between printing a variable vs printing a string
+print(device_id)
+print("m50923")
+print()
+
+# Use the type function to figure out the data type
+# Type
+device_id_2 = "h30943"
+data_type = type(device_id_2)
+print(data_type)
+print()
+
+# Reassigning a variable
+device_id_3 = "f3023"
+print("3rd Device ID:")
+print(device_id_3)
+print()
+print("Reassigned 3rd Device ID:")
+device_id_3 = "b30934"
+print(device_id_3)
+print()
